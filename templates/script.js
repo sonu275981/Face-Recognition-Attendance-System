9bcf991344fc8e9cd7aa171ea3a1e9b3f2663f76
trangb2005861@student.ctu.edu.vn <trangb2005861@student.ctu.edu.vn>
2023-11-07 01:49:48 +0700

    Trang firt commit

---
+++ templates/script.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+} );
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+} );
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
